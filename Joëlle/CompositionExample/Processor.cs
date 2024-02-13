5d70ca6d44a0ee149e933173653f760b2e1559cd
john doe <johndoe@example.com>
2024-02-13 09:17:00 +0100

    'assignment'

---
+++ Joëlle/CompositionExample/Processor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositionExample
+{
+    internal class Processor
+    {
+        public int clockSpeed;
+
+        public Processor(int clockSpeed)
+        {
+            this.clockSpeed = clockSpeed;
+        }
+
+        public void StartUp()
+        {
+            Console.WriteLine("Starting the processor...");
+        }
+    }
+}
