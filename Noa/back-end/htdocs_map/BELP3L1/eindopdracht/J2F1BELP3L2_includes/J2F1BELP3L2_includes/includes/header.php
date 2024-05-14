569c805c19406077c1edf39cc4408ba7dbdcbf71
Noa <99072699@mydavinci.nl>
2024-05-14 12:13:39 +0200

    backend leerpad 3

---
+++ Noa/back-end/htdocs_map/BELP3L1/eindopdracht/J2F1BELP3L2_includes/J2F1BELP3L2_includes/includes/header.php
@@ -0,0 +1,18 @@
+<!-- jouw HTML voor een Header komt hier... 
+Gebruik hier tenminste een header afbeelding en een menu
+Zorg dat je in het menu bij elk item een url parameter zet
+om te bepalen welke inhoud er ingeladen moet worden in je html
+-->
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <p></p>
+    </header>
+</body>
+</html>
